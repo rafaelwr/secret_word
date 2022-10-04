091805cb8f97383daf4894c58897e2a14ec02202
Teste <teste@saleplus.net.br>
2022-10-04 02:52:27 -0300

    Lógica do fim do game.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ const stages = [
   {id: 3, name: 'end'},
 ];
 
+const guessesQty = 3;
+
 const pickedCategoryAndWord = (words) => {
   // Pick category
   const categories = Object.keys(words[0]);
@@ -40,7 +42,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const startGame = () => {
@@ -77,10 +79,27 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter
       ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
     }
   }
 
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  useEffect(() => {
+    if(guesses <= 0) {
+      clearLetterStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);
   }
 
