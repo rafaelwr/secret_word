5b8af3a124f2b9291b5540cc332d7986a890f912
Teste <teste@saleplus.net.br>
2022-10-04 02:24:38 -0300

    Feito evento de inserção de letras.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
 import './Game.css';
+import { useState, useRef } from 'react';
 
 const Game = ({ 
     verifyLetter,
@@ -10,6 +11,18 @@ const Game = ({
     guesses,
     score
 }) => {
+    const [letter, setLetter] = useState('');
+    const letterInputRef = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        verifyLetter(letter);
+
+        setLetter('');
+        letterInputRef.current.focus();
+    }
+
     return (
         <div className="game">
             <p className="points">
@@ -32,12 +45,15 @@ const Game = ({
             </div>
             <div className="letterContainer">
                 <p>Tente adivinhar uma letra da palavra:</p>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
                         type="text"
                         name="letter"
                         maxLength="1"
                         required
+                        onChange={(e) => setLetter(e.target.value)}
+                        value={letter}
+                        ref={letterInputRef}
                     />
                     <button>Jogar!</button>
                 </form>
