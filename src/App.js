d7ffa5b847c4a80a2687c41b9693e1d3a00dc4e9
Teste <teste@saleplus.net.br>
2022-10-04 00:56:01 -0300

    Criado os componentes principais do game.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 // CSS
 import './App.css';
 
-//Components
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words.js';
+
+// Components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'},
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
+  const words = useState(wordsList);
+
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
