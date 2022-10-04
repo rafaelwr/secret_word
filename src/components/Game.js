19229ff9062fa0543cf4c8a608bef9273c0411f1
Teste <teste@saleplus.net.br>
2022-10-04 01:43:23 -0300

    Criação da tela principal do game.

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,35 @@ import './Game.css';
 
 const Game = ({ verifyLetter }) => {
     return (
-        <div>
-            <h1>Game</h1>
-            <button onClick={verifyLetter}>Finalizar</button>
+        <div className="game">
+            <p className="points">
+                <span>Pontuação</span>: 000
+            </p>
+            <h1>Adivinhe a palavra:</h1>
+            <h3 className="tip">
+                Dica sobre a palavra: <span>Dica...</span>
+            </h3>
+            <p>Você ainda tem 1 tentativa(s).</p>
+            <div className="wordContainer">
+                <span className="letter">A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className="letterContainer">
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form>
+                    <input
+                        type="text"
+                        name="letter"
+                        maxLength="1"
+                        required
+                    />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas:</p>
+                <span>a, </span>
+            </div>
         </div>
     )
 }
