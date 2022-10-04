81a9d6bc067dcec1a0f98c9fcd5bf57cafd1b15f
Teste <teste@saleplus.net.br>
2022-10-04 02:55:53 -0300

    Feita a tela e lógica do Game Over.

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
